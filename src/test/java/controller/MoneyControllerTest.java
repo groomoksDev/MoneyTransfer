037bec915d1f6f1e6cbfa218f36fbe05dd29b00a
groomoksDev <groomoksdev@gmail.com>
2019-06-17 01:43:10 +0300

    addProj

---
+++ src/test/java/controller/MoneyControllerTest.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.junit.Assert;
+import services.bl.AccountVerifier;
+import services.bl.AccountVerifierImpl;
+import services.bl.MoneyManager;
+import services.bl.MoneyManagerImpl;
+
+public class MoneyControllerTest {
+    MoneyController moneyController;
+
+    @org.junit.Test
+    public void sendMoney_Params_True() {
+        AccountVerifier accountVerifier = new AccountVerifierImpl();
+        MoneyManager moneyManager = new MoneyManagerImpl();
+        moneyController = new MoneyController(accountVerifier, moneyManager);
+        Assert.assertTrue(moneyController.sendMoney(1L,2L,3.0));
+    }
+
+    @org.junit.Test
+    public void getAccountMoneyInfo_Params_10() {
+        AccountVerifier accountVerifier = new AccountVerifierImpl();
+        MoneyManager moneyManager = new MoneyManagerImpl();
+        moneyController = new MoneyController(accountVerifier, moneyManager);
+        Assert.assertEquals(10.0, moneyController.getAccountMoneyInfo(1L), 0.0);
+    }
+}
